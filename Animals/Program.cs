e3837765f139c7b4d7d2d9d70cfe6b762ee4cca9
Laura Collins <laura.e.collins1@gmail.com>
2020-03-02 21:20:41 -0600

    classes

--- Animals/Program.cs
+++ Animals/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using Animals.Reptiles;
 
-namespace Animals
+namespace Animals //Bundle of classes
 {
-    class Program
+    class Program //Bundle of Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var americanAlligator = new Alligator(100);
+
+            americanAlligator.IsAquatic = true;
+
+            var africanAlligator = new Alligator(2);
+
+            africanAlligator.IsAquatic = false;
+
+            americanAlligator.Run(75);
+            africanAlligator.Run(4);
+
+            americanAlligator.Run(40);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Animals/Reptiles/Alligator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Reptiles
+{
+    internal class Alligator //"internal" means only accessable within project. "public" means accessable to anyone. "private" means only usable within class.
+    {
+        public bool IsAquatic { get; set; }
+        private int _stepGoal;
+        private int _stepCount;
+
+        public Alligator(int stepGoal)
+        {
+            IsAquatic = true;
+            _stepGoal = stepGoal;
+        }
+        public void Grunt()
+        {
+            if (IsAquatic)
+            {
+                Console.WriteLine("Bubble bubble bubble");
+            }
+            else
+            {
+                Console.WriteLine("GRRRRRR!");
+            }
+        }
+        public void Run(int steps) //Method signature
+        {
+            _stepCount += steps;
+
+            Console.WriteLine($"You ran {steps} steps.");
+
+            if (_stepCount >= _stepGoal)
+            {
+                Console.WriteLine("Congratulations, you met your goal!");
+            }
+        }
+    }
+}
